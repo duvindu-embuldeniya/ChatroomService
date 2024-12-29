84fe846588a636750e8c9dbfd3a89b69794e46e4
duvindu_embuldeniya <duvindu.personal@gmail.com>
2024-12-29 14:08:19 +0530

    key changed

--- chatroom_service/chatroom_service/settings.py
+++ chatroom_service/chatroom_service/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='duvindu.me@gmail.com'
-EMAIL_HOST_PASSWORD ='ijhn vygw rxxt ohqc'
+EMAIL_HOST_USER = os.environ.get('user')
+EMAIL_HOST_PASSWORD = os.environ.get('app_pass')
